2a1c144a28eefab9ce582ed9147eca657307e535
Nishant Tomar <nishanttomar21@gmail.com>
2021-01-06 00:44:12 +0530

    comments indentation

--- TicTacToe.py
+++ TicTacToe.py
@@ -5,21 +5,21 @@
 #############################################
 
 # Importing libraries
-from random import choice                                                                                                                                                       # To randomly select a position from an array (positions_left)
-from itertools import combinations                                                                                                                                          # To generate combinations from a list
+from random import choice                                                                                                                       # To randomly select a position from an array (positions_left)
+from itertools import combinations                                                                                                              # To generate combinations from a list
 
 # Global variables for the game (Initialization)
 playing_board = ["-","-","-","-","-","-","-","-","-"]
 fixed_positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 human_moves = []*5
 computer_moves = []*4
-positions_left = [1, 2, 3, 4, 5, 6, 7, 8, 9]                                                                                                                                  # Has positions left in array
-magic_positions_left = [1, 2, 3, 4, 5, 6, 7, 8, 9]                                                                                                                     # Has magic values in array 
-magic_square_board = [8, 3, 4, 1, 5, 9, 6, 7, 2]                                                                                                                    # Has magic values at a position
+positions_left = [1, 2, 3, 4, 5, 6, 7, 8, 9]                                                                                                    # Has positions left in array
+magic_positions_left = [1, 2, 3, 4, 5, 6, 7, 8, 9]                                                                                              # Has magic values in array 
+magic_square_board = [8, 3, 4, 1, 5, 9, 6, 7, 2]                                                                                                # Has magic values at a position
 player = "human"
-turn = 1                                                                                                                                                                                        # Odd -> Human, Even -> Computer
+turn = 1                                                                                                                                        # Odd -> Human, Even -> Computer
 won = False
-new_position = 0                                                                                                                                                                        # To set new calculated position      
+new_position = 0                                                                                                                                # To set new calculated position      
 
 # To add switch case to Python
 class switch:
@@ -33,8 +33,8 @@ class switch:
         return self.value in values
 
 # To display the board
-def displayBoard(arr):
-        print("\t" + str(arr[0]) + " | " + str(arr[1]) + " | " + str(arr[2]))                                                                                   # Only strings can be concatenated
+def displayBoard(arr):      
+        print("\t" + str(arr[0]) + " | " + str(arr[1]) + " | " + str(arr[2]))                                                                   # Only strings can be concatenated
         print("\t--------")
         print("\t" + str(arr[3]) + " | " + str(arr[4]) + " | " + str(arr[5]))
         print("\t--------")
@@ -53,8 +53,8 @@ def initializeGame():
 
 # To set the current player
 def setPlayer():
-    global player                                                                                                                                                                    # To reference global variable, as assignment will make it refer to local variable
-    player = "human" if (turn % 2) else "computer"                                                                                                         # Ternary Operator  (To set the player)
+    global player                                                                                                                               # To reference global variable, as assignment will make it refer to local variable
+    player = "human" if (turn % 2) else "computer"                                                                                              # Ternary Operator  (To set the player)
 
 # To generate a random value from the array inputs
 def generateRandomPosition():
@@ -63,15 +63,15 @@ def generateRandomPosition():
 # To update the playing board and dependent values of variables
 def addPositionToBoard(position):
     magic_square_value = magic_square_board[int(position)-1]
-    playing_board[int(position)-1] = "X" if (player == "human") else "O"                                                                                                # Ternary Operator (To add O or X to the player position)
-    positions_left.remove(int(position))                                                                                                                                                         # To remove the occupied position magic value from array
-    magic_positions_left.remove(magic_square_value)                                                                                                                              # To remove the occupied position from array
-    human_moves.append(magic_square_value) if (player == "human") else computer_moves.append(magic_square_value)     # Ternary Operator (To add the move's magic value to player_moves array)
-    increamentTurn()                                                                                                                                                                                         # Increament the turn
+    playing_board[int(position)-1] = "X" if (player == "human") else "O"                                                                        # Ternary Operator (To add O or X to the player position)
+    positions_left.remove(int(position))                                                                                                        # To remove the occupied position magic value from array
+    magic_positions_left.remove(magic_square_value)                                                                                             # To remove the occupied position from array
+    human_moves.append(magic_square_value) if (player == "human") else computer_moves.append(magic_square_value)                                # Ternary Operator (To add the move's magic value to player_moves array)
+    increamentTurn()                                                                                                                            # Increament the turn
 
 # To increament the turn value
 def increamentTurn():
-    global turn                                                                                                                                                                       # To reference global variable, as assignment will make it refer to local variable
+    global turn                                                                                                                                 # To reference global variable, as assignment will make it refer to local variable
     turn += 1
 
 # To check if player added a valid input position (within range, empty position and is digit)
@@ -80,8 +80,8 @@ def checkInput(value):
 
 # Take input from the player
 def takeInput():
-    human_input = input()                                                                                                                                                   # Take input from the player
-    input_passed = checkInput(human_input)                                                                                                                 # Checking the entered input
+    human_input = input()                                                                                                                       # Take input from the player
+    input_passed = checkInput(human_input)                                                                                                      # Checking the entered input
 
     # If input is valid, return that input
     if (input_passed):
@@ -98,7 +98,7 @@ def displayChanceInformation():
     if(player == "human"):
         # Loop till the input value is not valid
         while (True):
-            print("\n\nTurn " + str(turn) + " -> " + "You (Enter any position from " + str(positions_left) + "): ", end = ' ')                                               # (end = ' ') -> To remove new-line
+            print("\n\nTurn " + str(turn) + " -> " + "You (Enter any position from " + str(positions_left) + "): ", end = ' ')                  # (end = ' ') -> To remove new-line
             human_input = takeInput()
 
             if human_input:
@@ -128,16 +128,16 @@ def displayDrawStatement():
 # To check if a player has won or not
 def checkIfWon(arr):
     # Checking if co-linearity exists
-    for combination in combinations(human_moves if (player == "human") else computer_moves, 3):                        # Generating all unique combinations of length 3 from list
-        if (sum(combination) == 15):                                                                                                                                          # Checking if sum of 3 moves is 15
+    for combination in combinations(human_moves if (player == "human") else computer_moves, 3):                                                 # Generating all unique combinations of length 3 from list
+        if (sum(combination) == 15):                                                                                                            # Checking if sum of 3 moves is 15
             global won
             won = True
             break
 
 # Human chance to play
 def humanChance():
-    human_input = displayChanceInformation()                                                                                                                     # Printing and inputing turn information
-    addPositionToBoard(human_input)                                                                                                                                   # Adding the input position to playing board                                                                                                                        
+    human_input = displayChanceInformation()                                                                                                    # Printing and inputing turn information
+    addPositionToBoard(human_input)                                                                                                             # Adding the input position to playing board                                                                                                                        
 
     # If more than 3 chances are played, then check if won after playing the chance
     if (turn  > 3):
@@ -145,21 +145,21 @@ def humanChance():
 
 # Basic computer chance (Select random position and play)
 def computerChance():
-    displayChanceInformation()                                                                                                                                               # Printing information 
-    new_random_position = generateRandomPosition()                                                                                                      # Genarte a valid random position from the board
-    addPositionToBoard(new_random_position)                                                                                                                  # Adding the random position to playing board
+    displayChanceInformation()                                                                                                                  # Printing information 
+    new_random_position = generateRandomPosition()                                                                                              # Genarte a valid random position from the board
+    addPositionToBoard(new_random_position)                                                                                                     # Adding the random position to playing board
 
 # Check for winning position
 def checkWinningPosition(player):
     winning_position_exists = False
 
     # Checking if co-linearity exists
-    for combination in combinations(human_moves if (player == "human") else computer_moves, 2):                                                                      # Generating all unique combinations of length 3 from list
+    for combination in combinations(human_moves if (player == "human") else computer_moves, 2):                                                 # Generating all unique combinations of length 3 from list
         colinear_condition_variable = 15 - sum(combination)                                                                                                                                              # Conditions -> 1) 15 - (Sum of 2 positions) <= 9; 2) Winning position >= 1
 
-        if (colinear_condition_variable < 10 and colinear_condition_variable > 0 and  colinear_condition_variable in magic_positions_left):          # Checking colinear condition exists          
+        if (colinear_condition_variable < 10 and colinear_condition_variable > 0 and  colinear_condition_variable in magic_positions_left):     # Checking colinear condition exists          
             global new_position
-            new_position = magic_square_board.index(colinear_condition_variable) + 1                                                                                                    # Getting index of magic value (winning position)
+            new_position = magic_square_board.index(colinear_condition_variable) + 1                                                            # Getting index of magic value (winning position)
             winning_position_exists = True
             
             if (player == "computer"):
@@ -171,11 +171,11 @@ def checkWinningPosition(player):
     
 # To start playing the game (Tic Tac Toe)
 def startGame():
-    while (turn != 10):                                                                                                                                                                 # Loop till 9 chances (turns)
-        setPlayer()                                                                                                                                                                         # Set player at each iteration
+    while (turn != 10):                                                                                                                          # Loop till 9 chances (turns)
+        setPlayer()                                                                                                                              # Set player at each iteration
 
         # Switch case with condition of turn
-        with switch(turn) as case:                                                                                                                                                # Custom switch condition
+        with switch(turn) as case:                                                                                                               # Custom switch condition
             # Initial random chances
             if case(1, 2, 3):
                 # Chance of computer
@@ -186,7 +186,7 @@ def startGame():
                 else:
                     humanChance()       
                     
-                displayBoard(playing_board)                                                                                                                                  # Display the game board
+                displayBoard(playing_board)                                                                                                      # Display the game board
                 
             # Checking magic square for each chance       
             elif case(4, 5, 6, 7, 8, 9):
@@ -210,7 +210,7 @@ def startGame():
                     else:
                         computerChance()
 
-                displayBoard(playing_board)                                                                                                                                 # Display the game board
+                displayBoard(playing_board)                                                                                                       # Display the game board
 
             # Default case       
             else:
@@ -218,12 +218,12 @@ def startGame():
 
             # If any player won, break the loop 
             if (won):
-                displayWinningStatement()                                                                                                                                   # Display winning statement
+                displayWinningStatement()                                                                                                         # Display winning statement
                 break
 
     # If no one wins, then game draw
     if (not won):
-        displayDrawStatement()                                                                                                                                                  # Display draw statement
+        displayDrawStatement()                                                                                                                    # Display draw statement
 
 # So that this function doesn't run when some other file imports this file
 if __name__ == "__main__":
